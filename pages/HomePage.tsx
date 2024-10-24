67196b10019dae29b0ce7ef02c08cf6ac61e0f5b
Karim-Maftoun <kmaftoun@gmail.com>
2024-10-24 15:15:24 -0700

    home

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -21,18 +21,33 @@ export default function HomePage() {
         <div className={styles.detail}>
           <div className={styles.textContainer}>
             <div className={styles.name}>
-              <h1>Hi, I'm {name}</h1>
+              <h1 aria-label="Introduction">Hi, I'm Karim Maftoun</h1>
             </div>
 
             <div className={styles.bio}>
-              <p>{description}</p>
+            <h2 aria-label="About Karim">
+              I’m a computer science major and aspiring entrepreneur at UWCSE. I work on my own passion projects 
+              and have a sneaker reselling business. I also volunteer with the Washington Bus to raise political awareness 
+              on important issues in the Seattle area. Thank you for coming to my website.</h2>
             </div>
 
             <div className={styles['social-media']}>
-              <SocialIcon className={styles['social-icon']} url={LinkedinURL} />
-              <SocialIcon className={styles['social-icon']} url={GithubURL} />
-              <SocialIcon className={styles['social-icon']} url={InstagramURL}/>
-              <SocialIcon className={styles['social-icon']} url={TwitterURl} />
+              <SocialIcon className={styles['social-icon']} 
+              url={LinkedinURL} 
+              aria-label="LinkedIn Profile"
+               />
+              <SocialIcon className={styles['social-icon']} 
+              url={GithubURL} 
+              aria-label="GitHub Profile" 
+              />
+              <SocialIcon className={styles['social-icon']} 
+              url={InstagramURL} 
+              aria-label="Instagram Profile"
+              />
+              <SocialIcon className={styles['social-icon']} 
+              url={TwitterURl} 
+              aria-label="Twitter Profile" 
+              />
             </div>
           </div>
 
@@ -40,7 +55,11 @@ export default function HomePage() {
             <img
               className={styles.circleimg}
               src={'./picture.jpg'}
-              alt="profile picture"
+              alt="Profile picture of Karim Maftoun, standing outdoors on a sunny day, 
+              holding a beverage in a clear cup with a straw. They are wearing a dark t-shirt and light-colored shorts,
+              and are positioned in front of a blue building. The background includes a sidewalk with a advertising,
+               as well as a few people walking in the distance."
+              tabIndex="0"
             />
           </div>
         </div>
